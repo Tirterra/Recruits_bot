20da8ef65ee10cd5a18b8d08f070bac2ad8556d6
Tirterra <64198514+Tirterra@users.noreply.github.com>
2021-09-04 21:39:53 +0200

    fixed error with weight embed not displaying fastests runs

--- get_lvl.py
+++ get_lvl.py
@@ -11,198 +11,69 @@ except:
 
 def get_skill_lvl(player_xp, cap):
     
-    required_xp = {
-        50 : 1,	
-        175	: 2,
-        375	: 3,
-        675	: 4,
-        1175 : 5,	
-        1925 : 6,
-        2925 : 7,	
-        4425 : 8,
-        6425 : 9,
-        9925 : 10,	
-        14925 :	11,
-        22425 :	12,
-        32425 :	13,
-        47425 :	14,
-        67425 :	15,
-        97425 :	16,
-        147425 : 17,	
-        222425 : 18,
-        322425 : 19,
-        522425 : 20,	
-        822425 : 21,
-        1222425 : 22,	
-        1722425 : 23,
-        2322425 : 24,	
-        3022425	: 25,
-        3822425	: 26,
-        4722425	: 27,
-        5722425	: 28,
-        6822425	: 29,
-        8022425	: 30,
-        9322425	: 31,
-        10722425 : 32,	
-        12222425 : 33,	
-        13822425 : 34,	
-        15522425 : 35,	
-        17322425 : 36,
-        19222425 : 37,	
-        21222425 : 38,	
-        23322425 : 39,	
-        25522425 : 40,	
-        27822425 : 41,	
-        30222425 : 42,	
-        32722425 : 43,	
-        35322425 : 44,	
-        38072425 : 45,	
-        40972425 : 46,	
-        44072425 : 47,	
-        47472425 : 48,	
-        51172425 : 49,	
-        55172425 : 50,	
-        59472425 : 51,	
-        64072425 : 52,	
-        68972425 : 53,	
-        74172425 : 54,	
-        79672425 : 55,	
-        85472425 : 56,	
-        91572425 : 57,	
-        97972425 : 58,	
-        104672425 : 59,	
-        111672425 :	60,
-    }
-
+    DIR_PATH = os.path.dirname(__file__)
+    with open(DIR_PATH+r"\constants.json", "r") as file : required_xp = json.load(file)["skill_xp_to_level"]
     previous = 0
 
     for xp in required_xp.keys():
 
         level = required_xp[xp]
 
-        if player_xp >= xp : 
+        if player_xp >= int(xp) : 
             if level == cap:
-                return {"level": level, "overflow" : player_xp - xp}
+                return {"level": level, "overflow" : player_xp - int(xp)}
             else:
-                previous = xp
+                previous = int(xp)
         else:
-            level = level - 1 + (player_xp-previous) / (xp-previous)
+            level = level - 1 + (player_xp-previous) / (int(xp)-previous)
             return {"level": round(level, 2), "overflow" : 0}
 
 
 def get_slayer_lvl(player_xp):
 
-    required_xp = {
-        5 : 1,	
-        15 : 2,
-        200 : 3,	
-        1000 : 4,	
-        5000 : 5,	
-        20000 : 6,	
-        100000 : 7,	
-        400000 : 8,	
-        1000000 : 9,
-    }
+    DIR_PATH = os.path.dirname(__file__)
+    with open(DIR_PATH+r"\constants.json", "r") as file : required_xp = json.load(file)["slayer_xp_to_level"]
 
     previous = 0
 
     for xp in required_xp.keys():
         level = required_xp[xp]
-        if player_xp >= xp : 
+        if player_xp >= int(xp) : 
             if player_xp >= 1000000:
                 return {"level" : 9, "overflow" : player_xp - 1000000, "exp" : player_xp}
             else:
-                previous = xp
+                previous = int(xp)
         else:
-            level = level - 1 + (player_xp-previous) / (xp-previous)
+            level = level - 1 + (player_xp-previous) / (int(xp)-previous)
             return {"level" : round(level, 2), "overflow" : 0, "exp" : player_xp} 
 
 
 def get_dungeon_lvl(player_xp):
 
-    required_xp = {
-        50 : 1,	
-        125	: 2,
-        235	: 3,
-        395	: 4,
-        625	: 5,
-        955	: 6,
-        1425 : 7,	
-        2095 : 8,
-        3045 : 9,	
-        4385 : 10,	
-        6275 : 11,	
-        8940 : 12,	
-        12700 : 13,	
-        17960 : 14,	
-        25340 : 15,	
-        35640 :	16,
-        50040 :	17,
-        70040 :	18,
-        97640 :	19,
-        135640 : 20,	
-        188140 : 21,
-        259640 : 22,
-        356640 : 23,
-        488640 : 24,	
-        668640 : 25,	
-        911640 : 26,	
-        1239640 : 27,	
-        1684640	: 28,
-        2284640	: 29,
-        3084640	: 30,
-        4149640	: 31,
-        5559640	: 32,
-        7459640	: 33,
-        9959640	: 34,
-        13259640 : 35,	
-        17559640 : 36,	
-        23159640 : 37,	
-        30359640 : 38,	
-        39559640 : 39,	
-        51559640 : 40,
-        66559640 : 41,	
-        85559640 : 42,	
-        109559640 :	43,
-        139559640 :	44,
-        177559640 : 45,
-        225559640 :	46,
-        285559640 :	47,
-        360559640 :	48,
-        453559640 :	49,
-        569809640 :	50,
-    }
-
+    DIR_PATH = os.path.dirname(__file__)
+    with open(DIR_PATH+r"\constants.json", "r") as file : required_xp = json.load(file)["catacombs_xp_to_level"]
     previous = 0
 
     for xp in required_xp.keys():
 
         level = required_xp[xp]
 
-        if player_xp >= xp : 
+        if player_xp >= int(xp) : 
 
             if player_xp >= 569809640:
                 return {"level" : 50, "overflow" : player_xp - 569809640}
             else:
-                previous = xp
+                previous = int(xp)
 
         else:
-            level = level - 1 + (player_xp-previous) / (xp-previous)
+            level = level - 1 + (player_xp-previous) / (int(xp)-previous)
             return {"level" : round(level, 2), "overflow" : 0}
 
 
 def get_skills(res):
 
-    caps = {
-        "farming" : 60,
-        "combat" : 60,
-        "foraging" : 50,
-        "enchanting" : 60,
-        "alchemy" : 50,
-        "taming" : 50,
-        "fishing" : 50,
-        "mining" : 60,
-        }
+    DIR_PATH = os.path.dirname(__file__)
+    with open(DIR_PATH+r"\constants.json", "r") as file : caps = json.load(file)["caps"]
 
     skills = {
         "mining" : None,
